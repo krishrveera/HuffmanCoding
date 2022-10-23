7ebbe3df861ce56fc0c31249cae603d9dd14fd38
krishrveera <krishv@usf.edu>
2022-10-23 17:07:42 -0400

    Comments added to final code

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -25,7 +25,7 @@ std::string Huffman::pFind(char a, std::string path, HuffmanNode* curr) {
         return path;
     }
     else if (curr->isBranch()) {
-        return pFind(a, path+"1", curr->right) + pFind(a, path+"0", curr->left);
+        return pFind(a, path+"1", curr->right) + pFind(a, path+"0", curr->left); // recursive element that gives a 1 when it heads to the right and a 0 when it heads to the left
     }
     else {
         return "";
@@ -42,48 +42,48 @@ std::string Huffman::pFind(char a, std::string path, HuffmanNode* curr) {
 HuffmanKey::iterator find_key(HuffmanKey& hmk, char key) {
     for (auto pair = hmk.begin(); pair != hmk.end(); ++pair ) {
         if (pair->first == key)
-            return pair;
+            return pair; // returns the corresponding pair to the key being identified as the for loop loops through the hmk vector
     }
     return hmk.end();
 }
 
 /**
-*@brief compresses the string inputted 
+*@brief compresses the string inputted and returns a 
 *
 *@param std::string inputStr
 */
 
 
 std::string Huffman::compress(const std::string inputStr) {
-    std::map<char, size_t> s = this->parse(inputStr);
+    std::map<char, size_t> s = this->parse(inputStr); 
     HeapQueue<HuffmanNode*, HuffmanNode::Compare> pq;
     for (auto pair = s.begin(); pair != s.end(); ++pair ) {
-        pq.insert(new HuffmanNode(pair->first, pair->second));
-        std::cout<< pair->first << ": " << pair->second << ";";
+        pq.insert(new HuffmanNode(pair->first, pair->second)); // inserts a new HuffmanNode in the priority queue 
+        std::cout<< pair->first << ": " << pair->second << ";"; // prints out the values in the node given above
     }
 
-    std::map<char, std::string> bit_keys;
+    std::map<char, std::string> bit_keys; 
     while(pq.size() > 1) {
         // std::cout<<pq<<std::endl;
         HuffmanNode* l = pq.min();
         pq.removeMin();
         HuffmanNode* r = pq.min();
-        pq.removeMin();
-        HuffmanNode * p = new HuffmanNode('\0', l->getFrequency() + r->getFrequency()); 
-        p->left = l; p->right = r; r->parent = p; l->parent = p;
-        if(bit_keys.find(r->getCharacter()) == bit_keys.end()) 
-            bit_keys[r->getCharacter()] = "";
-        if(bit_keys.find(l->getCharacter()) == bit_keys.end()) 
-            bit_keys[l->getCharacter()] = "";
+        pq.removeMin(); 
+        HuffmanNode * p = new HuffmanNode('\0', l->getFrequency() + r->getFrequency()); // the new huffman node of the summed frequency of the two characters of the lowest frequency is being added back to the huffman tree after removing the said characters
+        p->left = l; p->right = r; r->parent = p; l->parent = p; // a branch is being created of the two characters with the lowest frequency 
+        if(bit_keys.find(r->getCharacter()) == bit_keys.end()) // ?
+            bit_keys[r->getCharacter()] = ""; // ?
+        if(bit_keys.find(l->getCharacter()) == bit_keys.end()) // ?
+            bit_keys[l->getCharacter()] = ""; // ?
           
-        populate_bit_keys(bit_keys, "0", l);
-        populate_bit_keys(bit_keys, "1", r);
+        populate_bit_keys(bit_keys, "0", l); // bit_keys is being populated with a 0 since the character was alloted the left branch
+        populate_bit_keys(bit_keys, "1", r); // bit_keys is being populated with a 1 since the character was alloted the right branch
 
-        pq.insert(p);
+        pq.insert(p); // the parent node is being added to the priority queue
     }
 
-    this->root = pq.min();
-    std::string returnString;
+    this->root = pq.min(); // ?
+    std::string returnString; 
     // std::cout<<"\n\nCodes:::";
     // for (auto& pair : bit_keys) {
     //     std::cout<< pair.first << ": " << pair.second << ";";
@@ -98,11 +98,11 @@ std::string Huffman::compress(const std::string inputStr) {
 
 void Huffman::populate_bit_keys(std::map<char, std::string> &bit_keys, std::string dir, HuffmanNode* curr) {
     if (curr == nullptr) return;
-    if (curr->isLeaf()) {
+    if (curr->isLeaf()) { // ?
         auto elem = bit_keys.find(curr->getCharacter());
         elem->second = dir + elem->second;
     }
-    else {
+    else { // ? I know this branches to the left or right if the current is detected not to be a leaf
         populate_bit_keys(bit_keys, dir, curr->left);
         populate_bit_keys(bit_keys, dir, curr->right);
     }
@@ -110,17 +110,17 @@ void Huffman::populate_bit_keys(std::map<char, std::string> &bit_keys, std::stri
 
 
 std::string Huffman::serializeTree() const {
-    return p_serializeTree(this->root);
+    return p_serializeTree(this->root); 
 }
 
 std::string Huffman::p_serializeTree(HuffmanNode* const curr) const {
     if (curr->isLeaf()) {
         std::string s = "L";
-        s+=curr->getCharacter();
-        return s;
+        s+=curr->getCharacter(); 
+        return s; // returns the character with a L if the current node is a leaf
     }
     else {
-        return p_serializeTree(curr->left) + p_serializeTree(curr->right) + "B";
+        return p_serializeTree(curr->left) + p_serializeTree(curr->right) + "B"; // returns the character with a B indicating that branching has occured
     }
 }
 
@@ -137,27 +137,28 @@ std::string Huffman::decompress(const std::string inputCode, const std::string s
     for (int i = 0; i < serializedTree.size(); i++) {
         if (serializedTree[i] == 'B') {
             HuffmanNode* right = nodeStack.popLeft();
-            HuffmanNode* left = nodeStack.popLeft();
+            HuffmanNode* left = nodeStack.popLeft(); // both are pop left since we are decompressing the serialized tree from left to right
             HuffmanNode* parent = new HuffmanNode('\0', 0, nullptr, left, right);
             right->parent = parent; left->parent = parent;
             nodeStack.pushLeft(parent);
         }
         else { //"L{Char}"
             i++;
-           nodeStack.pushLeft(new HuffmanNode(serializedTree[i], 0));
+           nodeStack.pushLeft(new HuffmanNode(serializedTree[i], 0));// this is when the serialized tree indicates that we have reached a leaf
         }
     }
 
     this->root = nodeStack.popLeft();
 
     #ifdef DEBUG
-    assert(nodeStack.isEmpty());
+    assert(nodeStack.isEmpty()); // ? what is assert?
     #endif
 
     #ifdef DEBUG
-    assert(this->serializeTree() == serializedTree);
+    assert(this->serializeTree() == serializedTree); // ?
     #endif
     
+    // decompressing logic when to go right and left
     std::string returnString = "";
     HuffmanNode* curr = this->root;
     for (int i =0; i < inputCode.size(); i++) {
@@ -175,12 +176,12 @@ std::string Huffman::decompress(const std::string inputCode, const std::string s
 
     #ifdef DEBUG
     std::cout<< returnString<<std::endl;
-    assert(curr == this->root);
+    assert(curr == this->root); // ?
     #endif
 
     if (curr->isLeaf()) {
         returnString+= curr->getCharacter();
-    } 
+    }  // ?
     
 
     return returnString;
@@ -191,6 +192,7 @@ std::string Huffman::decompress(const std::string inputCode, const std::string s
 *
 * @param inStr
 */
+
 std::map<char, size_t> Huffman::parse(const std::string inStr) {
     std::map<char, size_t> freqParser;
     for (int i = 0; i < inStr.size(); i++) {
